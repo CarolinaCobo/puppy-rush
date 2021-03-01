ecd70668c08240fade53f4f8d86ffd8ac5ebeb4f
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-01 09:45:51 +0000

    add assign random integer and random color to create board function

--- game.js
+++ game.js
@@ -3,12 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
      const width = 8
      const squares = []
 
-    
+const puppyColors = [
+    'red',
+    'yellow',
+    'orange',
+    'purple',
+    'green',
+    'blue'
+]     
 
 // Create board 
 function createBoard() {
     for (let i = 0; i < width*width; i++) {
-        const square = document.createElement('div')
+        const square = document.createElement('div') // creates new divs
+        let randomColor = Math.floor(Math.random() * puppyColors.length) // assign a random integer
+        square.style.backgroundColor = puppyColors[randomColor] // assign the random color
         grid.appendChild(square)
         squares.push(square)
     }
