ff905fb05c9a34957034c536104b9967e2a8f23b
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-01 21:34:57 +0000

    makes it draggable

--- game.js
+++ game.js
@@ -15,7 +15,9 @@ const puppyColors = [
 // Create board 
 function createBoard() {
     for (let i = 0; i < width*width; i++) {
-        const square = document.createElement('div') // creates new divs
+        const square = document.createElement('div') // creates new div
+        square.setAttribute('draggable', true) // makes it draggable
+
         let randomColor = Math.floor(Math.random() * puppyColors.length) // assign a random integer
         square.style.backgroundColor = puppyColors[randomColor] // assign the random color
         grid.appendChild(square)
