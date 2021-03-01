624b1981924edaf0b7d2c38801f85600314b4de4
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-01 09:33:31 +0000

    add event listener and create board

---
+++ game.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+     const grid = document.querySelector('.grid')
+     const width = 8
+     const squares = []
+
+    
+
+// Create board 
+function createBoard() {
+    for (let i = 0; i < width*width; i++) {
+        const square = document.createElement('div')
+        grid.appendChild(square)
+        squares.push(square)
+    }
+}
+createBoard()
+
+})
