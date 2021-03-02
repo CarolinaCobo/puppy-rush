56777df682ae916df63e280af2731b2bc95cc567
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-02 09:00:17 +0000

    valid moves and check if there's a row of three equal colors

--- game.js
+++ game.js
@@ -26,46 +26,76 @@ function createBoard() {
 }
 createBoard()
 
-// Drag the puppies. In built event event listeners in every event of dragging. 
-
+// Drag the puppies. 
     let colorBeingDragged;
     let colorBeingReplaced;
     let squareIdBeingDragged;
     let squareIdBeingReplaced; 
 
+    //In built event event listeners in every event of dragging. 
     squares.forEach ( square => square.addEventListener('dragstart', dragStart))
     squares.forEach ( square => square.addEventListener('dragend', dragEnd))
-    squares.forEach ( square => square.addEventListener('dragOver', dragOver))
-    squares.forEach ( square => square.addEventListener('dragEnter', dragEnter))
+    squares.forEach ( square => square.addEventListener('dragover', dragOver))
+    squares.forEach ( square => square.addEventListener('dragenter', dragEnter))
     squares.forEach ( square => square.addEventListener('dragLeave', dragLeave))
     squares.forEach ( square => square.addEventListener('drop', dragDrop))
 
     function dragStart() {
-        colorBeingDragged = this.style.backgroundColor
-        squareIdBeingDragged = parseInt(this.id)
-        console.log(colorBeingDragged)
+        colorBeingDragged = this.style.backgroundColor; // Will pick the color that is being dragged
+        squareIdBeingDragged = parseInt(this.id); //ParseInt will make it an integer
     }
 
-    function dragOver() {
-        
+    function dragOver(e) {
+        e.preventDefault() //Prevents it to do the default action
     }
 
-    function dragEnter() {
-        
+    function dragEnter(e) {
+        e.preventDefault() //Prevents it to do the default action        
     }
 
     function dragLeave() {
-        
+        console.log(this.id, 'dragover')
+    }  
+
+    function dragDrop() {
+        colorBeingReplaced = this.style.backgroundColor; 
+        squareIdBeingReplaced = parseInt(this.id);
+        this.style.backgroundColor = colorBeingDragged;
+        squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;    
     }
 
     function dragEnd() {
-        
+        console.log(this.id, 'dragend')
+
+        //Valid moves will only allow to move it around 
+        let validMoves = [
+            squareIdBeingDragged -1, 
+            squareIdBeingDragged -width, 
+            squareIdBeingDragged +1,
+            squareIdBeingDragged +width,
+        ]
+
+        let validMove = validMoves.includes(squareIdBeingReplaced);
+
+        if (squareIdBeingReplaced && validMove) {
+            squareIdBeingReplaced = null;  
+        } else if (squareIdBeingReplaced && !validMove) {   // if it's too far 
+            squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced; // Gives back its color to get back to the original spot
+            squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged; // Original square that will be back to the original color
+        } else squares [squareIdBeingDragged].style.backgroundColor = colorBeingDragged; //Color back to the original square
     }
 
-    function dragDrop() {
-        colorBeingReplaced = this.style.backgroundColor
-        squareIdBeingReplaced = parseInt(this.id)
-        
-    }
+// Checking for matches 
+
+// Check for row of three 
+
+function checkRowForThree() {
+    for (i=0; i < 61; i++) { //Max squares is 64 so we are using 61 as it will check if there are 3 matches checking from 61 to 64
+        let rowOfThree = [i, i+1, i+2]  
+        let decidedColor = squares[i].style.backgroundColor;
+        const isBlank = squares[i].style.backgroundColor === '';
+
+}
+
 
 })
