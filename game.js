6af9e7a33e41591b17f708c287759ac75fd8f36c
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-08 07:39:44 +0000

    Add new JS file for the contact form and link it to the HMTL.

--- game.js
+++ game.js
@@ -231,6 +231,29 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 
-
-
 })
+
+
+
+    // Code from EmailJS documentation 
+
+    const btn = document.getElementById('button');
+
+    document.getElementById('form')
+     .addEventListener('submit', function(event) {
+       event.preventDefault();
+    
+       const serviceID = 'default_service';
+       const templateID = 'template_tngtc2q';
+    
+       emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = 'Send';
+          alert('Sent! Thanks for your message will get back to you shortly : ');
+        }, (err) => {
+          btn.value = 'Send Email';
+          alert(JSON.stringify(err));
+        });
+    });
+
+   
