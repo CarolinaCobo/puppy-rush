2459c3d857b173ee69ca3f2fc7f8d2f512e4d191
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-31 08:13:59 +0100

    Format file and add add comments

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,40 +1,39 @@
-// Code from EmailJS documentation 
+// Code from EmailJS documentation and Slack Questions
 
-// identifying submit button and contact form variables 
 const btn = document.getElementById('button');
 const contactForm = document.getElementById('form');
 
 //Setting variables for serviceID and templateID
 const serviceID = 'default_service';
 const templateID = 'template_tngtc2q';
- 
+
 //Code from emailJS to set up the initial function
-contactForm.addEventListener('submit', function(event) {
-   event.preventDefault(); //preventing default submit 
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault(); //preventing default submit 
 
-   btn.value = 'Sending...'; //displaying new button value when sending 
+  btn.value = 'Sending...'; //displaying new button value when sending 
 
-   emailjs.sendForm(serviceID, templateID, this) //sending the form
+  emailjs.sendForm(serviceID, templateID, this) //sending the form
     .then(() => {
-    
+
       btn.value = 'Submit';
-      
+
       //get the name of the user from input field
-      let userName = document.getElementById('name'); 
-      
-      
+      let userName = document.getElementById('name');
+
+
       //alert shows to the user when the message has been sent 
       alert(`Thank you for getting in touch ${userName.value}. Your message has been sent! We will be in touch soon!`);
 
       //reset the form on submit 
-      contactForm.reset(); 
-     
+      contactForm.reset();
+
     }, (err) => {
-        //error message for the user if the email is not being sent
+      //error message if the email is not being sent
       btn.value = 'Submit';
-      alert(alert(JSON.stringify(err)`Sorry ${userName.value} something went wrong`));
+      alert(alert(JSON.stringify(err)`Sorry ${userName.value} something went wrong, please try again`));
       console.log(error);
     });
 
-    
+
 });
