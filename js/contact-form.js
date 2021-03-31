5def7b8e7950a2b874eabcc017e798aa8a71697d
Carolina Cobo <ccobo.dev@gmail.com>
2021-03-31 08:09:30 +0100

    Reset form when sent and includes user name in the alert when displays

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,20 +1,40 @@
 // Code from EmailJS documentation 
+
+// identifying submit button and contact form variables 
 const btn = document.getElementById('button');
+const contactForm = document.getElementById('form');
 
-document.getElementById('form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
+//Setting variables for serviceID and templateID
+const serviceID = 'default_service';
+const templateID = 'template_tngtc2q';
+ 
+//Code from emailJS to set up the initial function
+contactForm.addEventListener('submit', function(event) {
+   event.preventDefault(); //preventing default submit 
 
-    const serviceID = 'default_service';
-    const templateID = 'template_tngtc2q';
+   btn.value = 'Sending...'; //displaying new button value when sending 
 
-    emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-        btn.value = 'Send';
-        alert('Sent! Thanks for your message will get back to you shortly :) ');
-      }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
-      });
-  });
+   emailjs.sendForm(serviceID, templateID, this) //sending the form
+    .then(() => {
+    
+      btn.value = 'Submit';
+      
+      //get the name of the user from input field
+      let userName = document.getElementById('name'); 
+      
+      
+      //alert shows to the user when the message has been sent 
+      alert(`Thank you for getting in touch ${userName.value}. Your message has been sent! We will be in touch soon!`);
 
+      //reset the form on submit 
+      contactForm.reset(); 
+     
+    }, (err) => {
+        //error message for the user if the email is not being sent
+      btn.value = 'Submit';
+      alert(alert(JSON.stringify(err)`Sorry ${userName.value} something went wrong`));
+      console.log(error);
+    });
+
+    
+});
