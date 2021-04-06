12497ba2bf8564bd14c5cee2b1ea68fdb76c664f
Carolina Cobo <ccobo.dev@gmail.com>
2021-04-06 17:08:04 +0100

    Replace btn for button for accesibility reasons

--- assets/scripts/contact-form.js
+++ assets/scripts/contact-form.js
@@ -1,6 +1,6 @@
 // Code from EmailJS documentation and Slack Questions
 
-const btn = document.getElementById('button');
+const button = document.getElementById('button');
 const contactForm = document.getElementById('form');
 
 //Setting variables for serviceID and templateID
@@ -11,12 +11,12 @@ const templateID = 'template_tngtc2q';
 contactForm.addEventListener('submit', function (event) {
   event.preventDefault(); //preventing default submit 
 
-  btn.value = 'Sending...'; //displaying new button value when sending 
+  button.value = 'Sending...'; //displaying new button value when sending 
 
   emailjs.sendForm(serviceID, templateID, this) //sending the form
     .then(() => {
 
-      btn.value = 'Submit';
+      button.value = 'Submit';
 
       //get the name of the user from input field
       let userName = document.getElementById('name');
@@ -30,7 +30,7 @@ contactForm.addEventListener('submit', function (event) {
 
     }, (err) => {
       //error message if the email is not being sent
-      btn.value = 'Submit';
+      button.value = 'Submit';
       alert(alert(JSON.stringify(err)`Sorry ${userName.value} something went wrong, please try again`));
     });
 });
--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -308,7 +308,7 @@ document.addEventListener(newLocal, () => {
 
 
   // Restart the game
-  const restartButton = document.getElementById("restart-btn");
+  const restartButton = document.getElementById("restart-button");
 
   function restartGame() {
     const gameOverModal = document.getElementById('end-modal');
